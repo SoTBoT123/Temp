2eb7bf573219b456bafa9cdd76a28ecabe62179f
SoTBoT123 <124480215+SoTBoT123@users.noreply.github.com>
2023-12-07 21:00:53 -0800

    Create tempTest.js

---
+++ tempTest.js
@@ -0,0 +1,294 @@
+class Utils {
+                constructor() {
+                    // MATH UTILS:
+                    let mathABS = Math.abs,
+                        mathCOS = Math.cos,
+                        mathSIN = Math.sin,
+                        mathPOW = Math.pow,
+                        mathSQRT = Math.sqrt,
+                        mathATAN2 = Math.atan2,
+                        mathPI = Math.PI;
+                    let _this = this;
+                    // GLOBAL UTILS:
+                    this.round = function(n, v) {
+                        return Math.round(n * v) / v;
+                    };
+                    this.toRad = function(angle) {
+                        return angle * (mathPI / 180);
+                    };
+                    this.toAng = function(radian) {
+                        return radian / (mathPI / 180);
+                    };
+                    this.randInt = function(min, max) {
+                        return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+                    this.randFloat = function(min, max) {
+                        return Math.random() * (max - min + 1) + min;
+                    };
+                    this.lerp = function(value1, value2, amount) {
+                        return value1 + (value2 - value1) * amount;
+                    };
+                    this.decel = function(val, cel) {
+                        if (val > 0)
+                            val = Math.max(0, val - cel);
+                        else if (val < 0)
+                            val = Math.min(0, val + cel);
+                        return val;
+                    };
+                    this.getDistance = function(x1, y1, x2, y2) {
+                        return mathSQRT((x2 -= x1) * x2 + (y2 -= y1) * y2);
+                    };
+                    this.getDist = function(tmp1, tmp2, type1, type2) {
+                        let tmpXY1 = {
+                            x: type1 == 0 ? tmp1.x : type1 == 1 ? tmp1.x1 : type1 == 2 ? tmp1.x2 : type1 == 3 && tmp1.x3,
+                            y: type1 == 0 ? tmp1.y : type1 == 1 ? tmp1.y1 : type1 == 2 ? tmp1.y2 : type1 == 3 && tmp1.y3,
+                        };
+                        let tmpXY2 = {
+                            x: type2 == 0 ? tmp2.x : type2 == 1 ? tmp2.x1 : type2 == 2 ? tmp2.x2 : type2 == 3 && tmp2.x3,
+                            y: type2 == 0 ? tmp2.y : type2 == 1 ? tmp2.y1 : type2 == 2 ? tmp2.y2 : type2 == 3 && tmp2.y3,
+                        };
+                        return mathSQRT((tmpXY2.x -= tmpXY1.x) * tmpXY2.x + (tmpXY2.y -= tmpXY1.y) * tmpXY2.y);
+                    };
+                    this.getDirection = function(x1, y1, x2, y2) {
+                        return mathATAN2(y1 - y2, x1 - x2);
+                    };
+                    this.getDirect = function(tmp1, tmp2, type1, type2) {
+                        let tmpXY1 = {
+                            x: type1 == 0 ? tmp1.x : type1 == 1 ? tmp1.x1 : type1 == 2 ? tmp1.x2 : type1 == 3 && tmp1.x3,
+                            y: type1 == 0 ? tmp1.y : type1 == 1 ? tmp1.y1 : type1 == 2 ? tmp1.y2 : type1 == 3 && tmp1.y3,
+                        };
+                        let tmpXY2 = {
+                            x: type2 == 0 ? tmp2.x : type2 == 1 ? tmp2.x1 : type2 == 2 ? tmp2.x2 : type2 == 3 && tmp2.x3,
+                            y: type2 == 0 ? tmp2.y : type2 == 1 ? tmp2.y1 : type2 == 2 ? tmp2.y2 : type2 == 3 && tmp2.y3,
+                        };
+                        return mathATAN2(tmpXY1.y - tmpXY2.y, tmpXY1.x - tmpXY2.x);
+                    };
+                    this.getAngleDist = function(a, b) {
+                        let p = mathABS(b - a) % (mathPI * 2);
+                        return (p > mathPI ? (mathPI * 2) - p : p);
+                    };
+                    this.isNumber = function(n) {
+                        return (typeof n == "number" && !isNaN(n) && isFinite(n));
+                    };
+                    this.isString = function(s) {
+                        return (s && typeof s == "string");
+                    };
+                    this.kFormat = function(num) {
+                        return num > 999 ? (num / 1000).toFixed(1) + "k" : num;
+                    };
+                    this.sFormat = function(num) {
+                        let fixs = [
+                            { num: 1e3, string: "k" },
+                            { num: 1e6, string: "m" },
+                            { num: 1e9, string: "b" },
+                            { num: 1e12, string: "q" }
+                        ].reverse();
+                        let sp = fixs.find(v => num >= v.num);
+                        if (!sp) return num;
+                        return (num / sp.num).toFixed(1) + sp.string;
+                    };
+                    this.capitalizeFirst = function(string) {
+                        return string.charAt(0).toUpperCase() + string.slice(1);
+                    };
+                    this.fixTo = function(n, v) {
+                        return parseFloat(n.toFixed(v));
+                    };
+                    this.sortByPoints = function(a, b) {
+                        return parseFloat(b.points) - parseFloat(a.points);
+                    };
+                    this.lineInRect = function(recX, recY, recX2, recY2, x1, y1, x2, y2) {
+                        let minX = x1;
+                        let maxX = x2;
+                        if (x1 > x2) {
+                            minX = x2;
+                            maxX = x1;
+                        }
+                        if (maxX > recX2)
+                            maxX = recX2;
+                        if (minX < recX)
+                            minX = recX;
+                        if (minX > maxX)
+                            return false;
+                        let minY = y1;
+                        let maxY = y2;
+                        let dx = x2 - x1;
+                        if (Math.abs(dx) > 0.0000001) {
+                            let a = (y2 - y1) / dx;
+                            let b = y1 - a * x1;
+                            minY = a * minX + b;
+                            maxY = a * maxX + b;
+                        }
+                        if (minY > maxY) {
+                            let tmp = maxY;
+                            maxY = minY;
+                            minY = tmp;
+                        }
+                        if (maxY > recY2)
+                            maxY = recY2;
+                        if (minY < recY)
+                            minY = recY;
+                        if (minY > maxY)
+                            return false;
+                        return true;
+                    };
+                    this.containsPoint = function(element, x, y) {
+                        let bounds = element.getBoundingClientRect();
+                        let left = bounds.left + window.scrollX;
+                        let top = bounds.top + window.scrollY;
+                        let width = bounds.width;
+                        let height = bounds.height;
+                        let insideHorizontal = x > left && x < left + width;
+                        let insideVertical = y > top && y < top + height;
+                        return insideHorizontal && insideVertical;
+                    };
+                    this.mousifyTouchEvent = function(event) {
+                        let touch = event.changedTouches[0];
+                        event.screenX = touch.screenX;
+                        event.screenY = touch.screenY;
+                        event.clientX = touch.clientX;
+                        event.clientY = touch.clientY;
+                        event.pageX = touch.pageX;
+                        event.pageY = touch.pageY;
+                    };
+                    this.hookTouchEvents = function(element, skipPrevent) {
+                        let preventDefault = !skipPrevent;
+                        let isHovering = false;
+                        // let passive = window.Modernizr.passiveeventlisteners ? {passive: true} : false;
+                        let passive = false;
+                        element.addEventListener("touchstart", this.checkTrusted(touchStart), passive);
+                        element.addEventListener("touchmove", this.checkTrusted(touchMove), passive);
+                        element.addEventListener("touchend", this.checkTrusted(touchEnd), passive);
+                        element.addEventListener("touchcancel", this.checkTrusted(touchEnd), passive);
+                        element.addEventListener("touchleave", this.checkTrusted(touchEnd), passive);
+                        function touchStart(e) {
+                            _this.mousifyTouchEvent(e);
+                            window.setUsingTouch(true);
+                            if (preventDefault) {
+                                e.preventDefault();
+                                e.stopPropagation();
+                            }
+                            if (element.onmouseover)
+                                element.onmouseover(e);
+                            isHovering = true;
+                        }
+                        function touchMove(e) {
+                            _this.mousifyTouchEvent(e);
+                            window.setUsingTouch(true);
+                            if (preventDefault) {
+                                e.preventDefault();
+                                e.stopPropagation();
+                            }
+                            if (_this.containsPoint(element, e.pageX, e.pageY)) {
+                                if (!isHovering) {
+                                    if (element.onmouseover)
+                                        element.onmouseover(e);
+                                    isHovering = true;
+                                }
+                            } else {
+                                if (isHovering) {
+                                    if (element.onmouseout)
+                                        element.onmouseout(e);
+                                    isHovering = false;
+                                }
+                            }
+                        }
+                        function touchEnd(e) {
+                            _this.mousifyTouchEvent(e);
+                            window.setUsingTouch(true);
+                            if (preventDefault) {
+                                e.preventDefault();
+                                e.stopPropagation();
+                            }
+                            if (isHovering) {
+                                if (element.onclick)
+                                    element.onclick(e);
+                                if (element.onmouseout)
+                                    element.onmouseout(e);
+                                isHovering = false;
+                            }
+                        }
+                    };
+                    this.removeAllChildren = function(element) {
+                        while (element.hasChildNodes()) {
+                            element.removeChild(element.lastChild);
+                        }
+                    };
+                    this.generateElement = function(config) {
+                        let element = document.createElement(config.tag || "div");
+                        function bind(configValue, elementValue) {
+                            if (config[configValue])
+                                element[elementValue] = config[configValue];
+                        }
+                        bind("text", "textContent");
+                        bind("html", "innerHTML");
+                        bind("class", "className");
+                        for (let key in config) {
+                            switch (key) {
+                                case "tag":
+                                case "text":
+                                case "html":
+                                case "class":
+                                case "style":
+                                case "hookTouch":
+                                case "parent":
+                                case "children":
+                                    continue;
+                                default:
+                                    break;
+                            }
+                            element[key] = config[key];
+                        }
+                        if (element.onclick)
+                            element.onclick = this.checkTrusted(element.onclick);
+                        if (element.onmouseover)
+                            element.onmouseover = this.checkTrusted(element.onmouseover);
+                        if (element.onmouseout)
+                            element.onmouseout = this.checkTrusted(element.onmouseout);
+                        if (config.style) {
+                            element.style.cssText = config.style;
+                        }
+                        if (config.hookTouch) {
+                            this.hookTouchEvents(element);
+                        }
+                        if (config.parent) {
+                            config.parent.appendChild(element);
+                        }
+                        if (config.children) {
+                            for (let i = 0; i < config.children.length; i++) {
+                                element.appendChild(config.children[i]);
+                            }
+                        }
+                        return element;
+                    };
+                    this.checkTrusted = function(callback) {
+                        return function(ev) {
+                            if (ev && ev instanceof Event && (ev && typeof ev.isTrusted == "boolean" ? ev.isTrusted : true)) {
+                                callback(ev);
+                            } else {
+                                //console.error("Event is not trusted.", ev);
+                            }
+                        };
+                    };
+                    this.randomString = function(length) {
+                        let text = "";
+                        let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                        for (let i = 0; i < length; i++) {
+                            text += possible.charAt(Math.floor(Math.random() * possible.length));
+                        }
+                        return text;
+                    };
+                    this.countInArray = function(array, val) {
+                        let count = 0;
+                        for (let i = 0; i < array.length; i++) {
+                            if (array[i] === val) count++;
+                        }
+                        return count;
+                    };
+                    this.hexToRgb = function(hex) {
+                        return hex.slice(1).match(/.{1,2}/g).map(g => parseInt(g, 16));
+                    };
+                    this.getRgb = function(r, g, b) {
+                        return [r / 255, g / 255, b / 255].join(", ");
+                    };
+                }
+            };
